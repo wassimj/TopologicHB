14b1d472e95d6a94439496ef2650f71ad6166ebb
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-08 16:38:00 +0300

    Authentication Worked!

--- 01_app.py
+++ 01_app.py
@@ -299,28 +299,28 @@ else:
             st.session_state['refresh_token'] = refresh_token
         else:
             st.write("Error occurred : " ,response.status_code, response.text)
-    else:
-        account = get_account_from_token("speckle.xyz", token)
-        st.write("Account:", account)
+if st.session_state['refersh_token']
+    account = get_account_from_token("speckle.xyz", token)
+    st.write("Account:", account)
+    client = SpeckleClient(host="speckle.xyz")
+    client.authenticate_with_token(token)
+    try:
+        streams = getStreams(client)
+    except:
+        account = get_account_from_token("speckle.xyz", refresh_token)
+        st.write("ACCOUNT", account)
         client = SpeckleClient(host="speckle.xyz")
-        client.authenticate_with_token(token)
-        try:
-            streams = getStreams(client)
-        except:
-            account = get_account_from_token("speckle.xyz", refresh_token)
-            st.write("ACCOUNT", account)
-            client = SpeckleClient(host="speckle.xyz")
-            client.authenticate_with_token(refresh_token)
-            streams = getStreams(client)
-        stream_names = ["Select a stream"]
-        for aStream in streams:
-            stream_names.append(aStream.name)
-        option = st.selectbox(
-            'Select A Stream',
-            (stream_names))
-        if option != "Select a stream":
-            stream = streams[stream_names.index(option)-1]
-            st.write(option)
-            st.subheader("Preview Image")
-            st.components.v1.iframe(src="https://speckle.xyz/preview/"+stream.id, width=250,height=250)
-            st.components.v1.iframe(src="https://speckle.xyz/embed?stream="+stream.id+"&transparent=false", width=400,height=600)
+        client.authenticate_with_token(refresh_token)
+        streams = getStreams(client)
+    stream_names = ["Select a stream"]
+    for aStream in streams:
+        stream_names.append(aStream.name)
+    option = st.selectbox(
+        'Select A Stream',
+        (stream_names))
+    if option != "Select a stream":
+        stream = streams[stream_names.index(option)-1]
+        st.write(option)
+        st.subheader("Preview Image")
+        st.components.v1.iframe(src="https://speckle.xyz/preview/"+stream.id, width=250,height=250)
+        st.components.v1.iframe(src="https://speckle.xyz/embed?stream="+stream.id+"&transparent=false", width=400,height=600)
