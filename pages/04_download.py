86ac033b993cf9a116dea81ac1573c0ae0a846ce
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-14 11:51:07 +0100

    Debugging getting runs

--- pages/04_download.py
+++ pages/04_download.py
@@ -38,6 +38,7 @@ def download_sql(api_key: str, owner: str, project: str, job_id: str):
         target_folder: The folder where the output will be downloaded.
     """
     job = Job(owner, project, job_id, ApiClient(api_token=api_key))
+    st.write(job.name)
     st.write(job.runs)
     run = job.runs[0]
     run_id = run.id
