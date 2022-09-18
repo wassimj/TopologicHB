7312dcbab8a873bdfcd8060b837b4de6ad08c522
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-18 19:16:40 +0100

    Debugging

--- pages/04_download.py
+++ pages/04_download.py
@@ -1,8 +1,6 @@
-import zipfile
 import streamlit as st
-from pollination_streamlit.interactors import Job
-from pollination_streamlit.api.client import ApiClient
-import pollination_sdk
+from pollination_streamlit.selectors import get_api_client
+from pollination_streamlit_io import auth_user, select_account
 import json
 
 
@@ -42,6 +40,31 @@ def download_sql(owner: str, project: str, job_id: str, api_key: str):
 
 submit_button = None
 job_type = None
+
+api_client = get_api_client()
+
+#api_key = st.text_input('api_key', type='password')
+
+'''
+if api_key:
+
+st.header("Authenticated User")
+
+acol1, acol2 = st.columns(2)
+
+with acol1:
+    user = auth_user('auth-user', api_client)
+    if user is not None:
+        st.json(user, expanded=False)
+
+with acol2:
+    account = select_account('select-account', api_client) or ''
+    if account is not None and 'username' in account:
+        st.json(account, expanded=False)
+
+
+
+
 with st.form('download-result'):
     api_key = st.text_input('api_key', type='password')
     owner = st.text_input('owner')
@@ -68,3 +91,4 @@ if submit_button:
     #st.write(sql_url)
 
  
+ '''
